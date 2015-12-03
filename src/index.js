8849fb4fdd33b7e08ac1c4953d61ae267d1fb10b
Richard Hills <richard.hills@10to8.com>
2015-12-03 00:17:52 +0000

    Top level index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ export {default as layer} from './layer';
 export {default as source} from './source';
 export {default as feature} from './feature';
 export {default as map} from './map';
-export {default as ol-component} from './ol-component';
 export {default as view} from './view';
