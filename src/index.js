efd8fb3839c098f59b39cd47e744a31ba5cf3887
Richard Hills <richard.hills@10to8.com>
2015-12-03 00:16:55 +0000

    Rearranging stuff

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export {default as geom} from './geom';
+export {default as interaction} from './interaction';
+export {default as layer} from './layer';
+export {default as source} from './source';
+export {default as feature} from './feature';
+export {default as map} from './map';
+export {default as ol-component} from './ol-component';
+export {default as view} from './view';
