5ca8df0611252c9c0642c199bffa3d1b97721afa
Tamas Nepusz <ntamas@gmail.com>
2016-05-18 23:39:46 +0200

    re-tabbing map-quest.js

--- src/source/map-quest.js
+++ src/source/map-quest.js
@@ -3,18 +3,18 @@ import ol from 'openlayers';
 import OLComponent from '../ol-component';
 
 export default class MapQuest extends OLComponent {
-	constructor(props) {
-	  super(props);
-		this.source = new ol.source.MapQuest(this.props);
+  constructor(props) {
+    super(props);
+    this.source = new ol.source.MapQuest(this.props);
   }
 
-	componentDidMount() {
-	  this.context.layer.setSource(this.source);
-	}
+  componentDidMount() {
+    this.context.layer.setSource(this.source);
+  }
 }
 
 MapQuest.propTypes = {
-	layer: React.PropTypes.string.isRequired
+  layer: React.PropTypes.string.isRequired
 }
 
 MapQuest.contextTypes = {
