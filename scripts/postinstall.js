899a128dcd587ee2b781f2c7c44c8d5057a818a6
Tamas Nepusz <ntamas@gmail.com>
2016-05-20 13:07:51 +0200

    trying to support the old version of Node.js that is installed on Ubuntu by default

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,12 +1,19 @@
-var execSync = require('child_process').execSync
+var exec = require('child_process').exec
 var stat = require('fs').stat
-
-function exec(command) {
-  execSync(command, { stdio: [0, 1, 2] })
-}
 
 stat('lib', function(error, stat) {
   if (error || !stat.isDirectory()) {
-    exec('npm run compile')
+    exec('npm run compile', function(error, stdout, stderr) {
+      if (error) {
+        console.error(error)
+        
+      }
+      if (stdout) {
+        console.log(stdout)
+      }
+      if (stderr) {
+        console.log(stderr)
+      }
+    })
   }
 })
