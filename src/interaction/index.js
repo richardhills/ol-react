c8f4797af01ab78ea64b6c38f11a21d92ff99f6d
Tamas Nepusz <ntamas@gmail.com>
2016-06-13 18:09:11 +0200

    OLInteraction is now public

--- src/interaction/index.js
+++ src/interaction/index.js
@@ -5,4 +5,5 @@ export {default as DragZoom} from './drag-zoom';
 export {default as Draw} from './draw';
 export {default as Modify} from './modify';
 export {default as MouseWheelZoom} from './mouse-wheel-zoom';
+export {default as OLInteraction} from './ol-interaction';
 export {default as Select} from './select';
