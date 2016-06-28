e1065c6ddd84cbe5cd9683b930bb8071584d7063
Tamas Nepusz <ntamas@gmail.com>
2016-06-28 23:06:58 +0200

    fixed spelling of threshold in pinch-rotate.js

--- src/interaction/pinch-rotate.js
+++ src/interaction/pinch-rotate.js
@@ -5,13 +5,13 @@ import OLInteraction from './ol-interaction';
 export default class PinchRotate extends OLInteraction {
   createInteraction (props) {
     return new ol.interaction.PinchRotate({
-      treshold: props.treshold,
+      threshold: props.threshold,
       duration: props.duration
     })
   }
 }
 
 PinchRotate.propTypes = Object.assign({}, OLInteraction.propTypes, {
-  treshold: React.PropTypes.number,
+  threshold: React.PropTypes.number,
   duration: React.PropTypes.number
 })
