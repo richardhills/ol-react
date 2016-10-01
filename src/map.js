b298a0f5085647b2e6012e2143201b80158232ed
Nick Spacek <nick@spacek.ca>
2016-10-01 14:03:55 -0300

    Adds onChangeSize event for map

--- src/map.js
+++ src/map.js
@@ -9,9 +9,12 @@ export default class Map extends React.Component {
       loadTilesWhileAnimating: props.loadTilesWhileAnimating,
       loadTilesWhileInteracting: props.loadTilesWhileInteracting,
       interactions: props.useDefaultInteractions ? ol.interaction.defaults() : [],
-      controls: props.useDefaultControls ? ol.control.defaults() : [],
+      controls: props.useDefaultControls ? ol.control.defaults() : []
     })
 
+    if (props.onChangeSize) {
+      this.map.on('change:size', props.onChangeSize);
+    }
     if (this.props.onSingleClick) {
       this.map.on('singleclick', this.props.onSingleClick);
     }
@@ -58,11 +61,12 @@ export default class Map extends React.Component {
 Map.propTypes = {
   loadTilesWhileAnimating: React.PropTypes.bool,
   loadTilesWhileInteracting: React.PropTypes.bool,
+  onSingleClick: React.PropTypes.func,
+  onChangeSize: React.PropTypes.func,
   view: React.PropTypes.element.isRequired,
   useDefaultInteractions: React.PropTypes.bool.isRequired,
   useDefaultControls: React.PropTypes.bool.isRequired,
   focusOnMount: React.PropTypes.bool.isRequired,
-  onSingleClick: React.PropTypes.func,
 
   children: React.PropTypes.oneOfType([
     React.PropTypes.arrayOf(React.PropTypes.element),
