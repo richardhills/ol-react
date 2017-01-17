1a969282e662df95c6877c2f1f86c19189166118
Jono Rogers <jono@rallysafe.com.au>
2017-01-17 12:26:40 +0100

    Added geom.Point

--- src/geom/index.js
+++ src/geom/index.js
@@ -1,3 +1,4 @@
 export {default as Polygon} from './polygon';
 export {default as LineString} from './line-string';
 export {default as RawGeometry} from './raw-geometry';
+export {default as Point} from './point';
