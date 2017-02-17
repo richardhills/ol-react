32ddee4638f02ae1f7b38fe0a5f7facb7f474711
Jono Rogers <jono@rallysafe.com.au>
2017-02-17 18:11:00 +0100

    Reworked view to take default parameters, and exposed the fit method

--- src/view.js
+++ src/view.js
@@ -5,7 +5,13 @@ import OLComponent from './ol-component';
 export default class View extends OLComponent {
   constructor(props) {
     super(props);
-    this.view = new ol.View();
+    var opts = {
+      center: props.initialCenter,
+      resolution: props.initialResolution,
+      zoom: props.initialZoom,
+      rotation: props.initialRotation
+    };
+    this.view = new ol.View(opts);
   }
 
   onMoveEnd(event) {
@@ -19,18 +25,18 @@ export default class View extends OLComponent {
     }
   }
 
-  updateFromProps_(props) {
-    if (typeof props.center !== 'undefined') {
-      this.view.setCenter(props.center);
+  updateFromProps_(nextProps) {
+    if (typeof nextProps.center !== 'undefined') {
+      this.view.setCenter(nextProps.center);
     }
-    if (typeof props.rotation !== 'undefined') {
-      this.view.setRotation(props.rotation);
+    if (typeof nextProps.rotation !== 'undefined') {
+      this.view.setRotation(nextProps.rotation);
     }
 
-    if (typeof props.resolution !== 'undefined') {
-      this.view.setResolution(props.resolution);
-    } else if (typeof props.zoom !== 'undefined') {
-      this.view.setZoom(props.zoom);
+    if (typeof nextProps.resolution !== 'undefined') {
+      this.view.setResolution(nextProps.resolution);
+    } else if (typeof nextProps.zoom !== 'undefined') {
+      this.view.setZoom(nextProps.zoom);
     }
   }
 
@@ -44,15 +50,31 @@ export default class View extends OLComponent {
   componentWillReceiveProps(props) {
     this.updateFromProps_(props);
   }
+
+  fit(geometry, options) {
+    this.view.fit(geometry, options);
+  }
 }
 
 View.propTypes = {
-  center: React.PropTypes.arrayOf(React.PropTypes.number).isRequired,
+  center: React.PropTypes.arrayOf(React.PropTypes.number),
   resolution: React.PropTypes.number,
   zoom: React.PropTypes.number,
+  rotation: React.PropTypes.number,
+  initialCenter: React.PropTypes.arrayOf(React.PropTypes.number),
+  initialResolution: React.PropTypes.number,
+  initialZoom: React.PropTypes.number,
+  initialRotation: React.PropTypes.number,
   onResolutionChanged: React.PropTypes.func,
   onZoomChanged: React.PropTypes.func,
-  onCenterChanged: React.PropTypes.func
+  onCenterChanged: React.PropTypes.func,
+}
+
+View.defaultProps = {
+  initialCenter: [0, 0],
+  initialResolution: 10000,
+  initialZoom: 10,
+  initialRotation: 0
 }
 
 View.contextTypes = {
